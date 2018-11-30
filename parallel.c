d9e76f43c1eaf9c4e21b9ad87dc8de77853e9ef3
Jordan Weaver <jww@q.com>
2018-11-29 22:27:16 -0800

    added random number generator and array of work

--- parallel.c
+++ parallel.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <semaphore.h>
+#include <time.h>
 
 //-----------------------------------SEMAPHORES--------------------------------
 sem_t *make_semaphore (int n);
@@ -21,33 +22,52 @@ int sem_signal(sem_t *sem)
 void *check_malloc (int size);
 void perror_exit(char msg[]);
 
-
+//-----------------------------------THREADS------------------------------------
+//array of thread IDs
+pthread_t THREADS[2];
 
 void *lower_thread (void *ptr);
 void *upper_thread (void *ptr);
+
+//random number generator. generates numbers between and including min and max
+int gen_rand(int min, int max) {
+	return (rand() % (max - min + 1)) + min;
+}
+
+//global array of "work" to be done by the threads
+const int ARRAY_SIZE = 10000; //warning: array_size must not be negative, 0, 1, or 2
+int WORK_ARRAY[10000];
+
+
 
 //----------------------------------START MAIN-----------------------------------
 int main()
 {
 	printf("Hello world\n");
-	/*
-	//Create threads
-	for (i = 0; i < NUM_THINKERS; i++) {
-		pthread_create(&THINKERS[i].threadID, NULL, philosopher_action, (void *) &THINKERS[i]);
-	}
-	//Block until all threads are finished
-	for(i = 0; i < NUM_THINKERS; i++) {
-		pthread_join(THINKERS[i].threadID, NULL);
-	}
-	*/
 	void * ptr;
-	pthread_t THREADS[2];
+
+	//fill array with random work values
+	int i = 0;
+	for(i = 0; i < ARRAY_SIZE; i++) {
+		WORK_ARRAY[i] = gen_rand(0, 1000);
+	}
+
+	srand(time(NULL));
+	
+	//test single thread time to complete
+	for(i = 0; i < ARRAY_SIZE; i++) {
+		usleep(WORK_ARRAY[i]);
+		printf("Single thread, current element: %i, sleeping: %i\n", i, WORK_ARRAY[i]);
+	}
+	
+	//test thread timings based on a single split of the array
 	//create threads
 	pthread_create(&THREADS[0], NULL, lower_thread, (void *) ptr);
 	pthread_create(&THREADS[1], NULL, upper_thread, (void *) ptr);
 	
 	pthread_join(THREADS[0], NULL);
 	pthread_join(THREADS[1], NULL);
+	
 	
 	return 0;
 	
@@ -79,10 +99,20 @@ void perror_exit(char msg[])
 	exit (-1);
 }
 
+//lower thread function. started by pthread_create
 void *lower_thread (void *ptr) {
-	
+	int i = 0;
+	for(i = 0; i < ARRAY_SIZE / 2; i++) {
+		usleep(WORK_ARRAY[i]);
+		printf("Lower thread, current element: %i, sleeping: %i\n", i, WORK_ARRAY[i]);
+	}
 }
 
+//upper thread function. started by pthread_create
 void *upper_thread (void *ptr) {
-	
+	int i = 0;
+	for(i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++) {
+		usleep(WORK_ARRAY[i]);
+		printf("Upper thread, current element: %i, sleeping: %i\n", i, WORK_ARRAY[i]);
+	}
 }
